09844683a659865e2632f6d5f80d751e44f4b737
shuheng2007 <l.shuheng@stu.xjtu.edu.cn>
2025-07-26 10:31:06 +0800

    添加项目文件。

---
+++ 25summer/25summer.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+constexpr int MAX_N = 100;
+constexpr int MAX_W = 10000;
+
+using namespace std;
+
+int n, W;
+int w[MAX_N], v[MAX_N];
+
+int dp[MAX_N + 1][MAX_W + 1];
+
+static void solve()
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (j < w[i])
+                dp[i + 1][j] = dp[i][j];
+            else
+                dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
+        }
+    }
+}
+
+//int main()
+//{
+//    cin >> n;
+//    for (int i = 0; i < n; i++)
+//        cin >> w[i];
+//    for (int i = 0; i < n; i++)
+//        cin >> v[i];
+//    cin >> W;
+//    solve();
+//    cout << dp[n][W] << endl;
+//}
+
